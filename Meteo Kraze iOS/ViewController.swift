e6dabde49461d040c23a87074f880dae4ea68f2d
Guillaume Fourrier <guillaume.fourrier@epitech.eu>
2018-09-12 11:28:37 +0200

    Initial Commit

---
+++ Meteo Kraze iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meteo Kraze iOS
+//
+//  Created by Guillaume Fourrier on 12/09/2018.
+//  Copyright © 2018 Guillaume Fourrier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
